0360b838de292060f77664c6c7d979b0023123ff
mirzayogy <mirza.yogy@gmail.com>
2023-05-04 09:03:34 +0800

    toUpperCase, toLowerCase, dan substring

--- src/main/java/OperasiString.java
+++ src/main/java/OperasiString.java
@@ -8,5 +8,13 @@ public class OperasiString {
 
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
+
+        //B a n j a r b a r u
+        //0 1 2 3 4 5 6 7 9 10
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(1,5));
+
     }
 }
